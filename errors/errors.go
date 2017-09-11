49828223ffb2bcc4f365fe1bef1805315dcfd0bc
xtimeline <xtimeline@gmail.com>
2017-09-11 15:39:03 +0800

    errors & log

---
+++ errors/errors.go
@@ -0,0 +1,65 @@
+package errors
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	CodeError        = 50000000
+	CodeUnknownError = 99999999
+)
+
+func New(code int, message string, detail map[string]interface{}) error {
+	return E{
+		id:      time.Now().UnixNano(),
+		code:    code,
+		message: message,
+		detail:  detail,
+		logged:  new(bool),
+	}
+}
+
+type E struct {
+	id      int64
+	code    int
+	message string
+	detail  map[string]interface{}
+	logged  *bool
+}
+
+func (err E) Code() int {
+	return err.code
+}
+
+func (err E) Error() string {
+	if err.detail == nil {
+		return err.message
+	} else {
+		return fmt.Sprintf("%s: %v", err.message, err.detail)
+	}
+}
+
+func (err E) Message() string {
+	return err.message
+}
+
+func (err E) Detail() map[string]interface{} {
+	return err.detail
+}
+
+func (err E) Id() int64 {
+	return err.id
+}
+
+func (err E) IsLogged() bool {
+	return *err.logged
+}
+
+func (err E) MarkLogged() {
+	*err.logged = true
+}
+
+func (err E) IsError() bool {
+	return err.code >= CodeError
+}
